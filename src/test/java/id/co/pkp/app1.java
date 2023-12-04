ceb95d596f963d3912c51557663049367b193ff7
ibnufadilah <ibnufadilah07@gmail.com>
2023-12-04 11:44:00 +0700

    pop up

--- src/test/java/id/co/pkp/app1.java
+++ src/test/java/id/co/pkp/app1.java
@@ -292,4 +292,24 @@ public class app1 {
             playwright.close();
         }
 
+        @Test
+    @DisplayName("Pop-up")
+    public void popup(){
+            Playwright playwright = Playwright.create();
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Page page = browser.newPage();
+            page.navigate("http://autopract.com/selenium/popup/");
+
+            Page popUp = page.waitForPopup(() -> {
+
+                page.locator("//a[normalize-space()='Open Link in Popup']").click();
+
+            });
+            popUp.waitForLoadState();
+            System.out.println(popUp.title());
+            page.close();
+            browser.close();
+            playwright.close();
+        }
+
 }
