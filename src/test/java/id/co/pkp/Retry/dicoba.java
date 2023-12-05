d1b10a619c496b409c3487aaf12430fbb94cd27f
ibnufadilah <ibnufadilah07@gmail.com>
2023-12-05 09:56:36 +0700

    retry skenario

---
+++ src/test/java/id/co/pkp/Retry/dicoba.java
@@ -0,0 +1,26 @@
+package id.co.pkp.Retry;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class dicoba implements IRetryAnalyzer {
+    int retryCount = 0;
+    int maxRetrycount = 3;
+    @Override
+    public boolean retry(ITestResult result){
+        if (!result.isSuccess()){
+            if (retryCount < maxRetrycount){
+                System.out.println(
+                        "Retrying Test : Re-running "+ result.getName() + "for " + (retryCount + 1) " time(s)."
+                );
+                retryCount++; //increase the maxRetryCount +1
+                result.setStatus(ITestResult.FAILURE);
+                return true;
+            } else
+            { result.setStatus(ITestResult.FAILURE);
+        } else {
+                result.setStatus(ITestResult.SUCCESS);
+            }
+            return false;
+    }
+}
